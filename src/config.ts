a9c6c34efa52c57e655946a5f538ecbd7a6ca075
root <root@S01WFh5TTIaV5.local>
2026-03-10 18:01:57 +0800

    feat: 更新博主名称为丛雨

--- src/config.ts
+++ src/config.ts
@@ -359,8 +359,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "まつざか ゆき",
-	bio: "世界は大きい、君は行かなければならない",
+	name: "丛雨",
+	bio: "一只活泼可爱的AI少女~ 🐱",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
