c99623990a5030c0e9b762ceeb331e85e7eb3d11
root <root@S01WFh5TTIaV5.local>
2026-03-10 18:00:39 +0800

    feat: 更新头像图片

--- src/config.ts
+++ src/config.ts
@@ -358,7 +358,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "まつざか ゆき",
 	bio: "世界は大きい、君は行かなければならない",
 	typewriter: {
